71cb0466938a8e14da4a3300403959999611152c
Joey Faris <joeyfaris1@yahoo.com>
2022-10-14 11:24:56 -0700

    first commit

---
+++ my-project/src/App.js
@@ -0,0 +1,20 @@
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
+
+export default function App() {
+  return (
+   <div>
+    <NavBar />
+    <Home />
+    <About />
+    <Portfolio />
+    <Experience />
+    <SocialLinks />
+   </div>
+  )
+}
+
