368055bcbde9bac4fef04bb85aa1e2ae2f385875
Joey Faris <joeyfaris1@yahoo.com>
2022-10-16 18:57:06 -0700

    about edit

--- portfolio-site/src/components/About.jsx
+++ portfolio-site/src/components/About.jsx
@@ -8,13 +8,12 @@ const About = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
                 <p className='text-xl mt-20'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                I am a passionate and highly motivated full stack developer with experience using frontend and backend technologies such as Javascript, React, Node, Bootstrap, CSS (Tailwind CSS & Bootstrap), Ruby on Rails, Sinatra, Active Record, SQL and PostgreSQL. I love to build new projects and debugging. I am also committed to writing clean, readable, and scalable code.
                 </p>
                 <br />
 
                 <p className='text-xl'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+                I received my Bachelor's from Cal Poly San Luis Obispo in 2019 where I shortly after I worked in Real Estate and Sales. I wanted to change career paths into software engineering, where I recently graduated from Flatiron School's full stack software engineering boot camp. I would love to apply my problem solving and programming skills to help clients and companies develop and maintain their software and technology.</p>
         </div>
     </div>
   )
