fb1678ee7ec4982c11271ce1d47268377f4c55cb
Joey Faris <joeyfaris1@yahoo.com>
2022-10-24 20:44:31 -0700

    heading created

--- weather-site/src/App.js
+++ weather-site/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
 
 function App() {
+  //const [query, setQuery] = useState({q: 'berlin'})
+
+  // const formatBackground = () => {
+  //   if (!weather) return "from-cyan-700 to-blue-700";
+  //   const threshold = units === "metric" ? 20 : 60;
+  //   if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+
+  //   return "from-yellow-700 to-orange-700";
+  // };
+
   return (
-    <h1 className="text-xl text-center  underline">
-    Hello cdsc!
-  </h1>
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
+    <TopButtons/>
+    <Inputs/>
+    
+    <TimeAndLocation/>
+    </div>
   );
 }
 
