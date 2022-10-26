e375250d0d447ff6776b053ff8a0854def4e02f5
Joey Faris <joeyfaris1@yahoo.com>
2022-10-26 15:38:02 -0700

    weather api data

--- weather-site/src/App.js
+++ weather-site/src/App.js
@@ -4,6 +4,8 @@ import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+import getFormattedWeatherData from './services/weatherService';
 
 function App() {
   //const [query, setQuery] = useState({q: 'berlin'})
@@ -16,6 +18,13 @@ function App() {
   //   return "from-yellow-700 to-orange-700";
   // };
 
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({q: 'London'})
+    console.log(data)
+  }
+
+  fetchWeather()
+
   return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
     <TopButtons/>
@@ -23,6 +32,8 @@ function App() {
 
     <TimeAndLocation/>
     <TemperatureAndDetails/>
+    <Forecast title='hourly forecast' />
+    <Forecast title='daily forecast' />
     </div>
   );
 }
